31d099a158d37f83980ab845d152f3a846706741
Betina Andonova <betina_andonova@abv.bg>
2026-03-16 20:25:13 +0200

    fix category migration

--- database/migrations/2026_03_16_180803_add_slug_to_categories.php
+++ database/migrations/2026_03_16_180803_add_slug_to_categories.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+ 
 return new class extends Migration
 {
     /**
@@ -12,17 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->string('slug')->after('name')->nullable();
         });
     }
-
+ 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropColumn('slug');
         });
     }
 };
